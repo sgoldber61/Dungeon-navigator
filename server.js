975e2b5cd18e7320c646b9ddb4414aaa10b9bf20
Sam Goldberg <sgoldber61@gmail.com>
2018-02-27 17:29:11 -0800

    initial commit

--- server.js
+++ server.js
@@ -3,4 +3,9 @@ var app = express();
 
 app.use('/', express.static(`${__dirname}/client/build`));
 
+// express will serve up index.html if it doesn't recognize the route
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
